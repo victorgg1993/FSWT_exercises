c881f3d12e12c55e7bddead4237c9361c9acfa5b
lockpicker <vigomga1993@gmail.com>
2020-03-04 01:32:09 +0100

    movidos los datos de temporada fruta aquí, son arrays

---
+++ 1_Professional_HTML_Page/scripts/taula_temporada_fruta.js
@@ -0,0 +1,165 @@
+let titols_temporada = ['Tipo', 'Temporada hemisferio norte', 'Temporada hemisferio sur',];
+let dades_temporada = [
+    ['Albaricoque', 'Mayo-septiembre', 'Diciembre-febrero'],
+    ['Alquejenje', 'Junio-julio', 'Septiembre-noviembre'],
+    ['Arándano', 'Mayo-septiembre', 'Diciembre-febrero'],
+    ['Castaña', 'Noviembre-enero', 'Abril-febrero'],
+    ['Cereza', 'Junio-agosto', 'Noviembre-enero'],
+    ['Ciruela', 'Mayo-octubre', 'Diciembre-abril'],
+    ['Clementina', 'Noviembre-enero', 'Abril-febrero'],
+    ['Frambuesa', 'Junio-septiembre', 'Diciembre-marzo'],
+    ['Fresa', 'Junio-agosto', 'Octubre-febrero'],
+    ['Fresa silvestre', 'Agosto', 'Enero-febrero'],
+    ['Fruta de la pasión', 'Julio-marzo', 'Octubre-junio'],
+    ['Grosella negra', 'Junio-agosto', 'Diciembre-febrero'],
+    ['Grosella roja', 'Julio-agosto', 'Enero-febrero'],
+    ['Higo', 'Junio-septiembre', 'Diciembre-abril'],
+    ['Kiwi', 'Enero-agosto', 'Febrero-agosto'],
+    ['Lichi', 'Abril-junio', 'Noviembre-febrero'],
+    ['Lima', 'Semptiembre-diciembre', 'Enero-agosto'],
+    ['Limón', 'Enero-marzo', 'Todo el año'],
+    ['Mandarina', 'Noviembre-mayo', 'Abril-octubre'],
+    ['Mango', 'Marzo-septiembre', 'Octubre-marzo'],
+    ['Manzana', 'Julio-marzo', 'Diciembre-septiembre'],
+    ['Melocotón/nectarina', 'Junio-septiembre', 'Diciembre-abril'],
+    ['Melón', 'Julio-octubre', 'Octubre-febrero'],
+    ['Membrillo', 'Octubre-diciembre', 'Febrero-junio'],
+    ['Naranja', 'Enero-marzo', 'Todo el año'],
+    ['Papaya', 'Todo el año', 'Todo el año'],
+    ['Pera', 'Septiembre-enero', 'Enero-junio'],
+    ['Piña', 'Marzo-julio', 'Octubre-febrero'],
+    ['Plátano', 'Enero-diciembre', 'Octubre-agosto'],
+    ['Pomelo', 'Septiembre-noviembre', 'Junio-febrero'],
+    ['Ruibarbo', 'Enero-mayo', 'Febrero-agosto'],
+    ['Uva', 'Septiembre-octubre', 'Diciembre-mayo'],
+];
+/*
+			<tr class="tr_taules">
+				<td>Albaricoque
+				<td>Mayo-septiembre
+				<td>Diciembre-febrero
+			<tr class="tr_taules">
+				<td>Alquejenje
+				<td>Junio-julio
+				<td>Septiembre-noviembre
+			<tr class="tr_taules">
+				<td>Arándano
+				<td>Mayo-septiembre
+				<td>Diciembre-febrero
+			<tr class="tr_taules">
+				<td>Castaña
+				<td>Noviembre-enero
+				<td>Abril-febrero
+			<tr class="tr_taules">
+				<td>Cereza
+				<td>Junio-agosto
+				<td>Noviembre-enero
+			<tr class="tr_taules">
+				<td>Ciruela
+				<td>Mayo-octubre
+				<td>Diciembre-abril
+			<tr class="tr_taules">
+				<td>Clementina
+				<td>Noviembre-enero
+				<td>Abril-febrero
+			<tr class="tr_taules">
+				<td>Frambuesa
+				<td>Junio-septiembre
+				<td>Diciembre-marzo
+			<tr class="tr_taules">
+				<td>Fresa
+				<td>Junio-agosto
+				<td>Octubre-febrero
+			<tr class="tr_taules">
+				<td>Fresa silvestre
+				<td>Agosto
+				<td>Enero-febrero
+			<tr class="tr_taules">
+				<td>Fruta de la pasión
+				<td>Julio-marzo
+				<td>Octubre-junio
+			<tr class="tr_taules">
+				<td>Grosella negra
+				<td>Junio-agosto
+				<td>Diciembre-febrero
+			<tr class="tr_taules">
+				<td>Grosella roja
+				<td>Julio-agosto
+				<td>Enero-febrero
+			<tr class="tr_taules">
+				<td>Higo
+				<td>Junio-septiembre
+				<td>Diciembre-abril
+			<tr class="tr_taules">
+				<td>Kiwi
+				<td>Enero-agosto
+				<td>Febrero-agosto
+			<tr class="tr_taules">
+				<td>Lichi
+				<td>Abril-junio
+				<td>Noviembre-febrero
+			<tr class="tr_taules">
+				<td>Lima
+				<td>Semptiembre-diciembre
+				<td>Enero-agosto
+			<tr class="tr_taules">
+				<td>Limón
+				<td>Enero-marzo
+				<td>Todo el año
+			<tr class="tr_taules">
+				<td>Mandarina
+				<td>Noviembre-mayo
+				<td>Abril-octubre
+			<tr class="tr_taules">
+				<td>Mango
+				<td>Marzo-septiembre
+				<td>Octubre-marzo
+			<tr class="tr_taules">
+				<td>Manzana
+				<td>Julio-marzo
+				<td>Diciembre-septiembre
+			<tr class="tr_taules">
+				<td>Melocotón/nectarina
+				<td>Junio-septiembre
+				<td>Diciembre-abril
+			<tr class="tr_taules">
+				<td>Melón
+				<td>Julio-octubre
+				<td>Octubre-febrero
+			<tr class="tr_taules">
+				<td>Membrillo
+				<td>Octubre-diciembre
+				<td>Febrero-junio
+			<tr class="tr_taules">
+				<td>Naranja
+				<td>Enero-marzo
+				<td>Todo el año
+			<tr class="tr_taules">
+				<td>Papaya
+				<td>Todo el año
+				<td>Todo el año
+			<tr class="tr_taules">
+				<td>Pera
+				<td>Septiembre-enero
+				<td>Enero-junio
+			<tr class="tr_taules">
+				<td>Piña
+				<td>Marzo-julio
+				<td>Octubre-febrero
+			<tr class="tr_taules">
+				<td>Plátano
+				<td>Enero-diciembre
+				<td>Octubre-agosto
+			<tr class="tr_taules">
+				<td>Pomelo
+				<td>Septiembre-noviembre
+				<td>Junio-febrero
+			<tr class="tr_taules">
+				<td>Ruibarbo
+				<td>Enero-mayo
+				<td>Febrero-agosto
+			<tr class="tr_taules">
+				<td>Uva
+				<td>Septiembre-octubre
+				<td>Diciembre-mayo
+*/
