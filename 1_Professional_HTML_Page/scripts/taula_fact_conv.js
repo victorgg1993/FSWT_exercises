489b5b5e80a5bc216af7eebc1bc3e6de9801fcd6
lockpicker <vigomga1993@gmail.com>
2020-03-08 14:43:20 +0100

    añadidos arrays de otras tablas

---
+++ 1_Professional_HTML_Page/scripts/taula_fact_conv.js
@@ -0,0 +1,45 @@
+
+let titols_fact_conv = [
+    // medidas líquidas
+    ['Medidas líquidas'],           // 0
+    ['Unidad', 'Cantidad',],        // 1
+
+    // equivalentes mantequilla
+    ['Equivalentes mantequilla'],   // 2
+    ['Unidad', 'Onzas', 'Gramos'],  // 3
+    // ingredientes secos
+    ['Ingredientes secos'],         // 4
+    ['Ingrediente', '1 cucharadita', '1 cucharada', '1 taza'], // 5
+];
+
+let dades_fact_conv = [
+    // medidas líquidas ( 0 )
+    [
+        ['1 cucharadita', '5ml',],
+        ['1 cucharada', '15ml',],
+        ['1/4 taza', '60ml',],
+        ['1/3 taza', '75ml',],
+        ['1/2 taza', '125ml',],
+        ['1 taza', '250ml',],
+    ],
+    // equivalentes mantequilla ( 1 )
+    [
+        ['1 cucharadita', '0.5 oz', '15 g'],
+        ['4 cucharaditas', '2 oz', '60 g'],
+        ['1/4 de taza', '2 oz', '60 g'],
+        ['8 cucharaditas', '4 oz', '115 g'],
+        ['1/2 taza', '4 oz', '115 g'],
+        ['1 taza', '8 oz', '230 g'],
+    ],
+    // ingredientes secos  ( 2 )
+    [
+        ['Harina', '3 g', '8 g', '125 g'],
+        ['Azúcar extrafino', '4 g', '12 g', '195 g'],
+        ['Azúcar glas', '3 g', '8 g', '130 g'],
+        ['Levadura química', '4 g', '11 g', ''],
+        ['Sal marina fina', '5 g', '15 g' ,''],
+    ],
+];
+
+
+
