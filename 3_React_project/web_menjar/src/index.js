08b003c2e1d738b7d7940658ceb2713552887361
lockpicker <vigomga1993@gmail.com>
2020-05-21 01:33:26 +0200

    desde aquí se incrusta el objeto App en las labels main del index.html

--- 3_React_project/web_menjar/src/index.js
+++ 3_React_project/web_menjar/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import Inici from './pagines/inici';
+import App from './pagines/App';
 import * as serviceWorker from './serviceWorker';
 
-
 // inici
-ReactDOM.render(<React.StrictMode>    <Inici />  </React.StrictMode>, document.getElementsByTagName('main')[0]);
-//ReactDOM.render(<React.StrictMode>    <BotoInici />  </React.StrictMode>, document.getElementsByTagName('img')[0]);
-
+ReactDOM.render(<React.StrictMode>    <App />  </React.StrictMode>, document.getElementsByTagName('main')[0]);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
