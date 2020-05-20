696433489c1f414834ae39b11f44869b338a7d0d
lockpicker <vigomga1993@gmail.com>
2020-05-21 01:31:13 +0200

    a futuro: donde irá la información del plato

---
+++ 3_React_project/web_menjar/src/pagines/plat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import '../css/llistat_plats.css';
+
+function Plat() {
+
+  return (
+    <div className="Plat">
+      <b>plat! (falta implementar V:)</b>
+    </div>
+  );
+}
+
+export default Plat;
