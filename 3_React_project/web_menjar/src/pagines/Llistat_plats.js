d00d5fe09cfd68318f1138a83a1330dc78bdcd54
lockpicker <vigomga1993@gmail.com>
2020-05-20 18:27:57 +0200

    a fer: on anirà tota la gestió del llistat de plats

---
+++ 3_React_project/web_menjar/src/pagines/Llistat_plats.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/style.css';
+
+function Llistat_plats() {
+
+  return (
+    <div className="List_plats">
+      <b>plats</b>
+    </div>
+  );
+}
+
+export default Llistat_plats;
