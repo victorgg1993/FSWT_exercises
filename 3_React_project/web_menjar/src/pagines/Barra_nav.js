a4ae7e7087340d42843518f83d975d3b28787d71
lockpicker <vigomga1993@gmail.com>
2020-05-20 18:26:53 +0200

    possible barra de navegació on s'implementaran opcions de menjar halal, kosher, vegano, intoleràncies, etc

---
+++ 3_React_project/web_menjar/src/pagines/Barra_nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/style.css';
+
+function Barra_nav() {
+
+  return (
+    <div className="Barra_nav">
+      <b>Barra navegació</b>
+    </div>
+  );
+}
+
+export default Barra_nav;
+
