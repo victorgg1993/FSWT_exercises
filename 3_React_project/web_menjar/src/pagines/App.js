4b0ad4bc484d6ba35aa4ed00c4b64760d700a6a5
lockpicker <vigomga1993@gmail.com>
2020-05-29 01:11:25 +0200

    añadida barra de navegación

--- 3_React_project/web_menjar/src/pagines/App.js
+++ 3_React_project/web_menjar/src/pagines/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import Inici from './inici';
-import Llistat_plats from './llistat_plats';
+import LlistatPlats from './LlistatPlats';
 import Plat from './plat';
+import BarraNav from './BarraNav';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-    return (
 
+    return (
         <Router>
             <Switch>
                 <Inici path="/" exact component={Inici} />
-                <Llistat_plats path="/llistat_plats" exact component={Llistat_plats} />
-                <Plat path="/Plat" exact component={Plat} />
+
+                <Route path="/llistatPlats">
+                    <div className="div_llistat_plats">
+                        <BarraNav />
+                        <LlistatPlats />
+                    </div>
+                </Route>
+
+                <Route path="/plat">
+                    <div className="div_plat">
+                        <BarraNav />
+                        <Plat />
+                    </div>
+                </Route>
+
             </Switch>
         </Router>
     );
