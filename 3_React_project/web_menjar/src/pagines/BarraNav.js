2e64a833def16f4c2ad2e980c27f8273a4c9ebc7
lockpicker <vigomga1993@gmail.com>
2020-05-29 01:12:22 +0200

    añadido botón inicio

---
+++ 3_React_project/web_menjar/src/pagines/BarraNav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/nav_bar.css';
+
+function BarraNav() {
+
+  return (
+    <div className="Barra_nav">
+      <a href="/" className="boto_inici">
+        Inici
+      </a>
+    </div>
+  );
+}
+
+export default BarraNav;
+
