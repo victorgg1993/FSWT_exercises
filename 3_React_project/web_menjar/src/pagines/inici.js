e654ec0e4f46b7ba19fb2a7b344eac1de24ec14e
lockpicker <vigomga1993@gmail.com>
2020-05-29 01:13:25 +0200

    se añadió una clase y una id para el css

--- 3_React_project/web_menjar/src/pagines/inici.js
+++ 3_React_project/web_menjar/src/pagines/inici.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import '../css/index.css';
 import logo_principal from '../img/logo.png';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+//import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 
 function Inici() {
 
-  let ruta_plats = "/llistat_plats";
+  let ruta_plats = "/llistatPlats";
 
   return (
-    <Link to={ruta_plats}>
-      <img src={logo_principal} alt='Click per anar als plats' />
+    <Link to={ruta_plats}  className="class_main_img" >
+      <img id="id_main_img" src={logo_principal} alt='Click per anar als plats' />
     </Link>
   );
 }
